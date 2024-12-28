82dc9b03421b779ef8ea701b335f39ffbcf21bad
Eduardo Pozos Huerta <edupozoshuerta@gmail.com>
2024-12-27 18:45:04 -0600

    Guardar cambios grandes

--- include/syslin.h
+++ include/syslin.h
@@ -1,8 +1,24 @@
+// SPDX-License: GPL-2
+
+/**
+ *
+ * @author: Eduardo Pozos Huerta
+ * @file: syslin.h
+ * @date: 27/12/2024
+ *
+ * /
+
 #ifndef __SYSLIN__H__
 #define __SYSLIN__H__
 
-extern void sys_write(const char* msg, unsigned long length); // Optimizada para ser más rápida que puts
-extern void sys_open(const char* file_name, int flags, int mode); // Optimizada para alto rendimiento
+// Funciones para salida y entrada de datos
+extern void sys_write(const char* msg, unsigned long length);	// Optimizada para ser más rápida que puts
+
+// FUnciones para archivos
+extern void sys_open(const char* filename, int flags, int mode);	// Optimizada para alto rendimiento
+extern void sys_close(const char* filename);				// Optimizado para alto rendimiento
+
+// Funciones para procesos
 extern void sys_exit(void); // Optimizada para salir de forma rápida de un proceso
 
 #endif
